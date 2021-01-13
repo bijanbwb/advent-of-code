69732a32cd3d1db0dc81a5313ce590c4841bd7d1
Bijan Boustani <bijanbwb@gmail.com>
2021-01-13 16:37:29 -0500

    Starting Elixir Day 04

--- elixir/lib/advent_of_code/year_2015/day_04.ex
+++ elixir/lib/advent_of_code/year_2015/day_04.ex
@@ -1,5 +1,37 @@
 defmodule AdventOfCode.Year2015.Day04 do
-  def hello() do
-    :world
+  @brute_force_attempts 1_000_000
+
+  @spec run() :: [{Enum.element(), Enum.index()}]
+  def run() do
+    Range.new(1, @brute_force_attempts)
+    |> Enum.map(&generate_attempt/1)
+    |> Enum.with_index(1)
+    |> Enum.filter(fn {hash, _index} -> starts_with_five_zeros?(hash) end)
+  end
+
+  @doc """
+  Each attempt is the input and a stringified integer.
+
+  ## Example
+
+      iex> generate_attempt(1)
+      "yzbqklnj1"
+  """
+  @spec generate_attempt(attempt_number :: pos_integer()) :: String.t()
+  def generate_attempt(attempt_number) do
+    string = raw_input() <> Integer.to_string(attempt_number)
+
+    :crypto.hash(:md5, string)
+    |> Base.encode16(case: :lower)
+  end
+
+  @spec starts_with_five_zeros?(string :: String.t()) :: boolean()
+  def starts_with_five_zeros?(string) do
+    String.starts_with?(string, "00000")
+  end
+
+  @spec raw_input() :: String.t()
+  def raw_input() do
+    "yzbqklnj"
   end
 end
