c0cd118c9c2be8dc9c2be75c51ab8c4b5ceded69
Bijan <bijanbwb@gmail.com>
2021-04-09 10:48:19 -0400

    Add files for Day 10 2015

---
+++ elm/tests/AdventOfCode/Year2015/Day10Test.elm
@@ -0,0 +1,20 @@
+module AdventOfCode.Year2015.Day10Test exposing (suite)
+
+import AdventOfCode.Year2015.Day10 exposing (..)
+import Expect
+import Test exposing (Test)
+
+
+suite : Test
+suite =
+    Test.describe "Day10"
+        [ shouldWork "" ]
+
+
+shouldWork : String -> Test
+shouldWork input =
+    Test.test "shouldWork" <|
+        \_ ->
+            input
+                |> run
+                |> Expect.equal 0
